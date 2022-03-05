e54cbf9d20c7a9fa7ad7863f30538f202c0f731f
rubramriv73 <rubramriv73@gmail.com>
2022-03-05 14:15:56 +0100

    Modificadas constantes para que funcione en el servidor

--- app/Config/constantes.php
+++ app/Config/constantes.php
@@ -1,12 +1,25 @@
 <?php
-define("DBHOST",    "localhost");
-define("DBUSER",    "root");
-define("DBPASS",    "");
-define("DBNAME",    "superheroes");
-define("DBPORT",    3306);
+// define("DBHOST",    "localhost");
+// define("DBUSER",    "root");
+// define("DBPASS",    "");
+// define("DBNAME",    "superheroes");
+// define("DBPORT",    3306);
 
-define("DIRBASEURL", "/superheroes/public/index.php");
-define("DIRPUBLIC", "/superheroes/public");
+// define("DIRBASEURL", "/superheroes/public/index.php");
+// define("DIRPUBLIC", "/superheroes/public");
+
+// define("SHTOSHOW", 5);
+?>
+
+<?php
+define("DBHOST",    "localhost");
+define("DBUSER",    "2daw2122_rariru");
+define("DBPASS",    "usuario");
+define("DBNAME",    "2daw2122_rariru");
+define("DBPORT",    9118);
+
+define("DIRBASEURL", "/~rariru/superheroes/public/index.php");
+define("DIRPUBLIC", "/~rariru/superheroes/public");
 
 define("SHTOSHOW", 5);
 ?>
